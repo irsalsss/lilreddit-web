bca4840392b74d9ee43cf995089cc5660ec5fec2
Muhamad Irsal Firanyah <muhamadirsal@gmail.com>
2020-11-01 20:19:34 +0700

    login and logout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,7 +3,7 @@ import { Formik, Form } from 'formik';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { Box, Button } from '@chakra-ui/core';
-import { useRegisterMutation } from '../generated/graphql';
+import { useLoginMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 import { useRouter } from 'next/router'
 
@@ -11,18 +11,18 @@ interface registerProps {
 
 }
 
-const Register: React.FC<registerProps> = ({}) => {
+const Login: React.FC<{}> = ({}) => {
   const router = useRouter()
-  const [, register] = useRegisterMutation()
+  const [, login] = useLoginMutation()
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values)
-          if (response.data?.register.errors){
-            setErrors(toErrorMap(response.data.register.errors))
-          } else if (response.data?.register.user){
+          const response = await login({ options: values })
+          if (response.data?.login.errors){
+            setErrors(toErrorMap(response.data.login.errors))
+          } else if (response.data?.login.user){
             router.push("/")
           }
         }}
@@ -42,7 +42,7 @@ const Register: React.FC<registerProps> = ({}) => {
                 label="Password"
               />
             </Box>
-            <Button mt={4} type="submit" isLoading={isSubmitting} variantColor="teal">Register</Button>
+            <Button mt={4} type="submit" isLoading={isSubmitting} variantColor="teal">Login</Button>
           </Form>
         )}
       </Formik>
@@ -50,4 +50,4 @@ const Register: React.FC<registerProps> = ({}) => {
   );
 }
 
-export default Register;
+export default Login;
